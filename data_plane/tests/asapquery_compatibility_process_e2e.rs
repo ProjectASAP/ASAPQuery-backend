eff81f480a29bc7733f1bfe1a10835cc1e25dd3c
zz_y <zeyingz@umd.edu>
2026-09-10 21:53:30 -0600

    Format combined process regression modules

--- data_plane/tests/asapquery_compatibility_process_e2e.rs
+++ data_plane/tests/asapquery_compatibility_process_e2e.rs
@@ -21,10 +21,10 @@ use tokio::sync::Mutex;
 #[path = "support/erp_planning_process.rs"]
 mod erp_planning_process;
 
-#[path = "support/durable_summary_process.rs"]
-mod durable_summary_process;
 #[path = "support/distinct_planning_process.rs"]
 mod distinct_planning_process;
+#[path = "support/durable_summary_process.rs"]
+mod durable_summary_process;
 
 struct ChildGuard(Child);
 
